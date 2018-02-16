6b77e8d8960975c499f97ff7d321848226e1fec6
egrashina.gi <10Fev1989Gal>
2018-02-16 12:33:48 +0300

    Реазилован скрипт открытия модального окна по клику на кнопку и его закрытия по клику на другую кнопку появившуюся в модальном окне.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const dialog = document.querySelector('.dialog');
+document.querySelector('.show').addEventListener('click', ()=> {
+    dialog.showModal()
+});
+
+document.querySelector('.close').addEventListener('click', ()=> {
+    dialog.close();
+});
